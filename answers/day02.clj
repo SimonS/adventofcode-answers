bba368b3cc475afca75efb6d0cd3eda184b244b4
Simon Scarfe <simon.scarfe@gmail.com>
2015-12-02 16:48:49 +0000

    cleaned up day 2 a little based on preference

--- answers/day02.clj
+++ answers/day02.clj
@@ -3,19 +3,21 @@
        clojure.string/split-lines
        (map #(map read-string (clojure.string/split % #"x")))))
 
-(defn paper [l w h] (let [sides [(* l w) (* w h) (* h l)]]
+(defn paper [[l w h]] (let [sides [(* l w) (* w h) (* h l)]]
                       (+ (* 2 (reduce + sides))
                          (apply min sides))))
 
-(defn ribbon [l w h] (let [surface-area (* l w h )
-                           perimeter (reduce +
-                                             (map (partial * 2)
-                                                  (take 2 (sort [l w h]))))]
-                       (+ surface-area perimeter)))
+(defn ribbon [dimensions]
+  (let [surface-area (apply * dimensions)
+        perimeter (->> (sort dimensions)
+                       (take 2)
+                       (map (partial * 2))
+                       (reduce +))]
+    (+ surface-area perimeter)))
 
 (defn total-data [func]
-  (reduce #(+ %1 (apply func (vec %2)))
-          0 normalised-data))
+  (->> (map func normalised-data)
+       (reduce +)))
 
 ;; Part 1
 (total-data ribbon)
