d25803e11142d9ced15215e273fddcb226393d81
Simon Scarfe <simon.scarfe@gmail.com>
2015-12-03 10:58:46 +0000

    generalised function and returned for 2 santas

--- answers/day03.clj
+++ answers/day03.clj
@@ -1,15 +1,29 @@
+(def directions (slurp "answers/inputs/day03.txt"))
+
+(defn get-delivered-houses [position, destinations, instructions]
+  (if (= (count instructions) 0)
+    destinations
+    (let [current-instruction (first instructions)
+          new-position (case current-instruction
+                         \> (update-in position [0] inc)
+                         \< (update-in position [0] dec)
+                         \^ (update-in position [1] inc)
+                         \v (update-in position [1] dec))]
+      (recur new-position
+             (conj destinations new-position)
+             (rest instructions)))))
+
+(defn directions->houses
+  "when given directions, returns set of house coordinates"
+  [directions]
+  (get-delivered-houses [0 0] #{[0 0]} directions))
+
 ;; Part 1
 
-((fn [position, destinations, instructions]
-   (if (= (count instructions) 0)
-     (count destinations)
-     (let [current-instruction (first instructions)
-           new-position (case current-instruction
-                          \> (update-in position [0] inc)
-                          \< (update-in position [0] dec)
-                          \^ (update-in position [1] inc)
-                          \v (update-in position [1] dec))]
-       (recur new-position
-              (conj destinations new-position)
-              (rest instructions)))))
-  [0 0] #{[0 0]} (slurp "answers/inputs/day03.txt"))
+(count (directions->houses directions))
+
+;; Part 2
+
+(count (clojure.set/union
+  (directions->houses (take-nth 2 directions))
+  (directions->houses (take-nth 2 (rest directions)))))
