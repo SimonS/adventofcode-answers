5733ccf549e4cf5d9215c8841e74e2b826197586
Simon Scarfe <simon.scarfe@gmail.com>
2015-12-04 19:50:47 +0000

    generalised coin finder for variable leading zeros

--- answers/day04.clj
+++ answers/day04.clj
@@ -10,13 +10,21 @@
         padding (apply str (repeat (- size (count sig)) "0"))]
     (str padding sig)))
 
-(def is-advent-coin? #(-> (md5 %)
-                          (subs 0 5)
-                          (= "00000")))
+(defn is-advent-coin?
+  [leading-zeros key]
+  (-> (md5 key)
+      (subs 0 leading-zeros)
+      (= (apply str (repeat leading-zeros \0)))))
 
-(defn find-advent-coin [k]
+(defn find-advent-coin [n k]
   (subs (first
-          (filter is-advent-coin?
+          (filter (partial is-advent-coin? n)
                   (map #(str k %)
                        (iterate inc 100000))))
         (count k)))
+
+;; Part 1
+(find-advent-coin 5 "yzbqklnj")
+
+;; Part 2 (this one takes a few seconds)
+(find-advent-coin 6 "yzbqklnj")
