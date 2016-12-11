7727848d7599dbb5b1ec92d2257c5eabd7222aea
SimonS <simon.scarfe@gmail.com>
2016-12-11 22:00:40 +0000

    day 04 part 2

--- 2016/day04.js
+++ 2016/day04.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 const inputFile = fs.readFileSync('input/day04.txt', 'utf8');
 
 let sanitisedInput = inputFile.split('\n').map(s => s.split(/(\d+)/)).map(a => {
-  return {name: a[0].replace(/-/g, ''), id: parseInt(a[1]), checksum: a[2]}
+  return {name: a[0], id: parseInt(a[1]), checksum: a[2]}
 });
 
-isRealRoom = room => {
-  let checksum = Object.entries(room.name.split('').sort().reduce((strs, letter) => {
+const isRealRoom = room => {
+  let checksum = Object.entries(room.name.replace(/-/g, '').split('').sort().reduce((strs, letter) => {
     // get letter counts
     strs[letter] ? strs[letter]++ : strs[letter] = 1;
     return strs
@@ -23,3 +23,17 @@ isRealRoom = room => {
 
 // sum real rooms
 console.log(sanitisedInput.filter(isRealRoom).reduce((total, x) => total + x.id, 0));
+
+const rotate = x => x == ' ' ? x : String.fromCharCode(x.charCodeAt(0)+1 > 'z'.charCodeAt(0) ? 'a'.charCodeAt(0) : x.charCodeAt(0)+1);
+
+// this is nasty and not very functional, but I'm sick of looking at this problem now.
+const rotx = (str, x) => {
+    for (i = 0; i < x; i++) {
+        str = rotate(str);
+    }
+    return str;
+}
+
+const rotStringX = (str, x) => str.replace(/-/g, ' ').split('').map(letter => rotx(letter, x)).join('')
+
+console.log(sanitisedInput.map(x => [rotStringX(x.name, x.id), x.id]).filter(x => x[0].indexOf('north') !== -1));
