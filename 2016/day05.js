72cddea4dcddb2bf92fcea0de4c58e27c46bfb16
SimonS <simon.scarfe@gmail.com>
2016-12-12 00:11:28 +0000

    day 5 part 2

--- 2016/day05.js
+++ 2016/day05.js
@@ -6,17 +6,31 @@ let pass = '';
 
 const isInteresting = hash => hash.slice(0,5) === '00000';
 
-const getNext = () => {
+let getNext = () => {
     while (!isInteresting(md5(input + srcN))) {
         srcN++
     }
-    return md5(input + srcN)[5]
+    return md5(input + srcN).slice(5, 7)
 }
 
 while (pass.length < 8) {
-    pass += getNext();
+    pass += getNext()[0];
     srcN++;
 }
 
 // takes 23.9 seconds. Should probably look into multithreaded node.
 console.log(pass);
+
+pass = {0: false, 1: false, 2: false, 3: false, 4: false, 5: false, 6: false, 7: false};
+
+while (!Object.values(pass).every(x => x !== false)) {
+    const next = getNext();
+
+    if (pass[next[0]] === false) {
+        pass[next[0]] = next[1];
+    }
+
+    srcN++;
+}
+
+console.log(Object.values(pass).join(''));
