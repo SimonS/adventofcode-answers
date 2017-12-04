e17576fe3f7e804dcadde24061675151f1c6b094
SimonS <simon.scarfe@gmail.com>
2017-12-04 18:42:25 +0000

    formatting

--- 2017/day04.clj
+++ 2017/day04.clj
@@ -5,21 +5,21 @@
        str/split-lines))
 
 (defn tokenize [word]
-    (str/split word #" "))
+  (str/split word #" "))
 
-(defn is-unique? [lst] 
-    (= (count (set lst)) (count lst)))
+(defn is-unique? [lst]
+  (= (count (set lst)) (count lst)))
 
 (defn password-has-unique-words? [passphrase]
-    (is-unique? (tokenize passphrase)))
+  (is-unique? (tokenize passphrase)))
 
 ; pt 1:
 (count (filter password-has-unique-words? normalised-data))
 
 (defn password-has-no-anagrams? [passphrase]
-    (->> (tokenize passphrase)
-         (map sort)
-         is-unique?))
+  (->> (tokenize passphrase)
+       (map sort)
+       is-unique?))
 
 ; pt 2:
 (count (filter password-has-no-anagrams? normalised-data))
