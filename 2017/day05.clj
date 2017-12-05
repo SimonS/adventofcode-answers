690449aee01d0a9f43031eea827b2d5794455b6c
SimonS <simon.scarfe@gmail.com>
2017-12-05 19:35:44 +0000

    optimised and done part 2

--- 2017/day05.clj
+++ 2017/day05.clj
@@ -1,24 +1,31 @@
 (require '[clojure.string :as str])
 
 (def normalised-data
-    (->> (slurp "2017/inputs/day05.txt")
-         str/split-lines
-         (map read-string)))
+  (->> (slurp "2017/inputs/day05.txt")
+       str/split-lines
+       (mapv read-string)))
 
+(defn find-exit-maker [modifying-func]
+  (fn [instructions]
+    (loop [ins instructions
+           curs 0
+           cnt 0]
+      (if (or (< curs 0) (>= curs (count ins)))
+        cnt
+        (recur
+         (modifying-func ins curs)
+         (+ (nth ins curs 1) curs)
+         (inc cnt))))))
+
+; pt 1:
 (defn increment-at [l n]
-  (map
-   #(if (= (first %) n) (inc (last %)) (last %))
-   (map-indexed vector l)))
+  (update l n inc))
 
-(defn find-exit [instructions]
-  (loop [ins instructions
-         curs 0
-         cnt 0]
-    (if (or (< curs 0) (>= curs (count ins)))
-      cnt
-      (recur
-       (increment-at ins curs)
-       (+ (nth ins curs 1) curs)
-       (inc cnt)))))
+(time ((find-exit-maker increment-at) normalised-data))
+; => "Elapsed time: 544.141253 msecs"
 
-(find-exit normalised-data)
+(defn increment-or-decrement-at [l n]
+  (update l n #(if (> % 2) (dec %) (inc %))))
+
+(time ((find-exit-maker increment-or-decrement-at) normalised-data))
+; => "Elapsed time: 36745.40914 msecs"
