4bda8fb33ca497016c3db5d51be399544bb07aba
SimonS <simon.scarfe@bbc.co.uk>
2018-12-03 00:23:48 +0000

    refactor

--- 2018/day01.js
+++ 2018/day01.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 
 const inputFile = fs.readFileSync('inputs/day01.txt', 'utf8');
 
-const part1 = inputFile.split('\n').map((s) => parseInt(s, 10)).reduce((a, b) => a + b, 0);
+const nums = inputFile.split('\n').map((s) => parseInt(s, 10));
+
+const part1 = nums.reduce((a, b) => a + b, 0);
 
 console.log(part1);
