c6feecf9e18b7c4f4c0ad477e7c84866528f55d8
SimonS <simon.scarfe@bbc.co.uk>
2018-12-03 20:13:37 +0000

    day 3 part 2

--- 2018/day03.js
+++ 2018/day03.js
@@ -3,12 +3,14 @@ const fs = require('fs');
 const inputFile = fs.readFileSync('inputs/day03.txt', 'utf8');
 
 const claimSpecs = inputFile.split('\n')
-    .map(s => s.split('@ ')[1])
+    .map(s => s.split('@ '))
     .map(s => {
-        const [xy, wh] = s.split(': ');
+        const [id, dimensions] = s
+        const [xy, wh] = dimensions.split(': ');
         const [x, y] = xy.split(',').map(x => parseInt(x, 10))
         const [w, h] = wh.split('x').map(x => parseInt(x, 10))
         return {
+            id,
             x,
             y,
             w,
@@ -27,19 +29,30 @@ for (let i = 0; i < rect.length; i++) {
     rect[i] = new Array(Math.max(...claimSpecs.map(spec => spec.b)))
 }
 
-rect.forEach(row => row.fill(0))
-
 const fillRect = (claim) => {
     for (let x = claim.x; x < (claim.x + claim.w); x++) {
         for (let y = claim.y; y < (claim.y + claim.h); y++) {
-            rect[y][x]++;
+            if (rect[y][x] === undefined) {
+                rect[y][x] = new Set([]);
+            }
+
+            rect[y][x].add(claim.id);
         }
     }
 }
 
 claimSpecs.forEach(fillRect);
 
-const totalOverSubscribed = rect.map(row => row.filter(col => col > 1).length)
-    .reduce((a, b) => a + b, 0)
+const overSubscribed = rect.map(row => row.filter(col => col.size > 1))
+const overSubscribedNums = overSubscribed.map(row => row.length)
+const totalOverSubscribed = overSubscribedNums.reduce((a, b) => a + b, 0)
 
 console.log(`part 1: ${totalOverSubscribed}`);
+
+// I actually intended to use Set arithmatic here, but es6 Sets are so primitive, it
+// was unlikely to finish running in time for AOC 2019.
+const allIDs = new Set(claimSpecs.map(spec => spec.id))
+overSubscribed.forEach(row => row.forEach(col => [...col].forEach(id => allIDs.delete(id))))
+
+const uniqueID = [...allIDs][0];
+console.log(`part 2: ${uniqueID}`);
