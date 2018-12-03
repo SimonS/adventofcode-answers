0a9e29d7fc0ff2f3f36bd1913b385539a3a45952
SimonS <simon.scarfe@bbc.co.uk>
2018-12-03 00:42:10 +0000

    added part 2 answer

--- 2018/day01.js
+++ 2018/day01.js
@@ -6,4 +6,20 @@ const nums = inputFile.split('\n').map((s) => parseInt(s, 10));
 
 const part1 = nums.reduce((a, b) => a + b, 0);
 
-console.log(part1);
+console.log(`Part 1 answer: ${part1}`);
+
+// warning - completely non-functional solution ahoy. I'm behind and playing catch up.
+
+let foundNumbers = new Set();
+let currentFrequency = 0
+
+let i = 0;
+
+while (!foundNumbers.has(currentFrequency)) {
+    foundNumbers.add(currentFrequency);
+    currentFrequency += nums[i];
+
+    i = i < nums.length - 1 ? i + 1 : 0
+}
+
+console.log(`Part 2 answer: ${currentFrequency}`);
