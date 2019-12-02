a2ee5892792a957f87aa5986a8c45050bfc7f215
Simon Scarfe <scarfs03@mc-s093092.local>
2019-12-02 21:48:12 +0000

    hacked together a nasty recursive part 2

--- 2019/day01.js
+++ 2019/day01.js
@@ -1,7 +1,21 @@
 const fs = require('fs');
 
 const inputFile = fs.readFileSync('inputs/day01.txt', 'utf8');
-const nums = inputFile.split('\n').map((s) => Math.floor(parseInt(s, 10) / 3) - 2);
-const part1 = nums.reduce((a, b) => a + b, 0);
+const nums = inputFile.split('\n').map(s => parseInt(s, 10));
+
+const calculateFuel = (n) => Math.floor(n / 3) - 2
+
+const part1 = nums.map(calculateFuel).reduce((a, b) => a + b, 0);
 
 console.log(`Part 1 answer: ${part1}`);
+
+const calculateAllFuels = (acc, i) => {
+    const fuelCost = calculateFuel(i)
+    if (fuelCost < 0) return acc
+    
+    return calculateAllFuels(acc + fuelCost, fuelCost)
+}
+
+const part2 = nums.map(n => calculateAllFuels(0, n)).reduce((a, b) => a + b, 0);
+
+console.log(`Part 2 answer: ${part2}`);
