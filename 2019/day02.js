131fb2e2df1884e6383a491fcceaf8866107b9c1
Simon Scarfe <scarfs03@mc-s093092.local>
2019-12-04 13:10:32 +0000

    bruteforce day 2

--- 2019/day02.js
+++ 2019/day02.js
@@ -3,22 +3,45 @@ const fs = require('fs');
 const inputFile = fs.readFileSync('inputs/day02.txt', 'utf8');
 let nums = inputFile.split(',').map(s => parseInt(s, 10));
 
-nums[1] = 12;
-nums[2] = 2;
+const processIntCode = (intCode, noun = 12, verb = 2) => {
+    let i = 0;
 
-let i = 0;
-while(i < nums.length && nums[i] !== 99) {
-    let opCode = nums[i];
-    
-    switch (opCode) {
-        case 1:
-            nums[nums[i + 3]] = nums[nums[i + 1]] + nums[nums[i + 2]]
-        break;
-        case 2:
-            nums[nums[i + 3]] = nums[nums[i + 1]] * nums[nums[i + 2]]
-        break;
+    intCode[1] = noun;
+    intCode[2] = verb;
+
+    while(i < intCode.length && intCode[i] !== 99) {
+        let opCode = intCode[i];
+        
+        switch (opCode) {
+            case 1:
+                intCode[intCode[i + 3]] = intCode[intCode[i + 1]] + intCode[intCode[i + 2]]
+            break;
+            case 2:
+                intCode[intCode[i + 3]] = intCode[intCode[i + 1]] * intCode[intCode[i + 2]]
+            break;
+        }
+        i += 4;
     }
-    i += 4;
+
+    return intCode[0];
 }
 
-console.log(`Part 1 answer: ${nums[0]}`);
+console.log(`Part 1 answer: ${processIntCode([...nums])}`);
+
+const target = 19690720;
+let actual = 0;
+
+let x = 0;
+let y;
+
+while (actual !== target && x < 100) {
+    y = 0;
+    x++;
+    
+    while (actual !== target && y < 100) {
+        y++;
+        actual = processIntCode([...nums], x, y)
+    }
+}
+
+console.log(`Part 2 answer: ${100*x+y}`);
