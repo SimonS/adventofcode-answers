6e41fcdd6f7f306f08d2ace62b3a6afc855bf378
Simon Scarfe <simon.scarfe@gmail.com>
2019-12-05 23:15:38 +0000

    My regex sucks, so this isn't pretty, but it works

--- 2019/day04.js
+++ 2019/day04.js
@@ -3,7 +3,18 @@ let eligiblePasswordCount = 0;
 let password = 231832;
 
 const isOrdered = str => str.split("").sort().join("") === str;
-const hasRepeats = str => /(.)\1/.test(str);
+const hasRepeats = str => {
+    let letters = str.split("");
+    let counts = {};
+    letters.forEach(letter => {
+        if (!(letter in counts)) {
+            counts[letter] = 1;
+        } else {
+            counts[letter]++;
+        }
+    });
+    return !!Object.values(counts).filter(count => count === 2).length
+};
 
 const isEligible = pw => isOrdered(pw) && hasRepeats(pw);
 
