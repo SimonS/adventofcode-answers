11e7aa5123ab0a9af5fcf9e3dd738c51cc32510a
Simon Scarfe <simon.scarfe@bbc.co.uk>
2020-12-05 18:07:19 +0000

    make row and col share same logic

--- 2020/day05.js
+++ 2020/day05.js
@@ -2,31 +2,18 @@ const seats = fs
   .readFileSync(__dirname + "/inputs/day05.txt", "utf8")
   .split("\n");
 
-const getSeatNumber = (seat) => {
-  const row = seat.slice(0, 7);
-  const col = seat.slice(7);
-  return (
-    row
-      .split("")
-      .reduce(
-        ([min, max], instruction) =>
-          instruction === "B"
-            ? [Math.ceil((min + max) / 2), max]
-            : [min, Math.floor((min + max) / 2)],
-        [0, 127]
-      )[0] *
-      8 +
-    col
-      .split("")
-      .reduce(
-        ([min, max], instruction) =>
-          instruction === "R"
-            ? [Math.ceil((min + max) / 2), max]
-            : [min, Math.floor((min + max) / 2)],
-        [0, 8]
-      )[0]
-  );
-};
+const numberSplitter = (instructions, targetLetter, upperBounds) =>
+  Array.from(instructions).reduce(
+    ([min, max], instruction) =>
+      instruction === targetLetter
+        ? [Math.ceil((min + max) / 2), max]
+        : [min, Math.floor((min + max) / 2)],
+    [0, upperBounds]
+  )[0];
+
+const getSeatNumber = (seat) =>
+  numberSplitter(seat.slice(0, 7), "B", 127) * 8 +
+  numberSplitter(seat.slice(7), "R", 7);
 
 const sortedSeats = seats.map(getSeatNumber).sort((a, b) => a - b);
 console.log(sortedSeats[sortedSeats.length - 1]);
