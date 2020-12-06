b713abf1a6dfd223db0b567535242ff39aabc18d
Simon Scarfe <simon.scarfe@bbc.co.uk>
2020-12-06 13:53:23 +0000

    part 2

--- 2020/day06.js
+++ 2020/day06.js
@@ -1,6 +1,19 @@
 const groups = fs
   .readFileSync(__dirname + "/inputs/day06.txt", "utf8")
-  .split("\n\n")
-  .map((s) => s.split("").filter((ch) => ch !== "\n"));
+  .split("\n\n");
 
-console.log(groups.map((group) => new Set(group).size).reduce((a, b) => a + b));
+const uniqueAnswers = groups
+  .map((s) => s.split("").filter((ch) => ch !== "\n"))
+  .map((group) => new Set(group).size);
+console.log(uniqueAnswers.reduce((a, b) => a + b));
+
+const sharedAnswers = groups.map(
+  (group) =>
+    group.split("\n").reduce((shared, current) =>
+      shared
+        .split("")
+        .filter((ch) => current.indexOf(ch) > -1)
+        .join("")
+    ).length
+);
+console.log(sharedAnswers.reduce((a, b) => a + b));
