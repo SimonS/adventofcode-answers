b5301fecc1bbce5ef9e796593d9ee977f16bbd9a
Simon Scarfe <simon.scarfe@cinch.co.uk>
2022-12-01 12:55:24 +0000

    get rid of targets

--- 2022/day1/target/debug/deps/day1-a6c91060de33eef8.d
+++
@@ -1,6 +0,0 @@
-/Users/simonscarfe/playground/projects/adventofcode-answers/2022/day1/target/debug/deps/day1-a6c91060de33eef8: src/main.rs src/../data.txt
-
-/Users/simonscarfe/playground/projects/adventofcode-answers/2022/day1/target/debug/deps/day1-a6c91060de33eef8.d: src/main.rs src/../data.txt
-
-src/main.rs:
-src/../data.txt:
