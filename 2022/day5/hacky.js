70d71391564e4adbfd037bc201d4c32b025eb0cc
Simon Scarfe <simon.scarfe@cinch.co.uk>
2022-12-05 20:39:36 +0000

    day 5 part 1

---
+++ 2022/day5/hacky.js
@@ -0,0 +1,47 @@
+const input = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`;
+
+const [setup, instructions] = input.split("\n\n");
+
+const getBuckets = (i) => {
+  const stacks = (i.split("\n")[0].length + 1) / 4;
+  const buckets = new Array(stacks).fill().map((_) => []);
+
+  const processLine = (line) => {
+    for (let i = 0; i < (line.length + 1) / 4; i++) {
+      const char = line[i * 4 + 1].trim();
+      if (char.length) buckets[i].push(char);
+    }
+  };
+
+  i.split("\n").forEach(processLine);
+
+  buckets.map((stack) => stack.reverse());
+  return buckets;
+};
+
+const buckets = getBuckets(setup.split("\n 1")[0]);
+
+const processInstruction = (line) => {
+  const groups = line.match(
+    /move (?<count>\d+) from (?<from>\d+) to (?<to>\d+)/
+  ).groups;
+
+  for (let i = 0; i < groups.count; i++) {
+    let item = buckets[groups.from - 1].pop();
+    buckets[groups.to - 1].push(item);
+  }
+};
+
+instructions.split("\n").forEach(processInstruction);
+
+const part1 = buckets.map((bucket) => bucket.pop()).join("");
+
+console.log(part1);
